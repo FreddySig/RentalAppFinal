a69d66aa01b151f76a1a00f27501caccf6c31a6d
FreddySig <60980367+FreddySig@users.noreply.github.com>
2020-02-13 14:31:12 -0800

    Update RentalLocations.java

--- RentalAppFinal/src/RentalLocations.java
+++ RentalAppFinal/src/RentalLocations.java
@@ -7,59 +7,56 @@ public class RentalLocations implements Serializable {
 	private static final long serialVersionUID = 8743507069010815384L;
 	private String name; // rental location name,cities for locations
 	private int id;
-	private String type = "car";
-	private final int vehicles = 50;
-	private int rentedVehicles;
-
-	private double dailyRate;
+	private String type = "";
 	private int zip;
+	private List<Vehicles> inventory;
 
 
-	// total vehicles = 50
-	public RentalLocations(String name, int rentedVehicles, int id, int zip) {
+	public RentalLocations(String name, int id, int zip) {
 		this.name = name;
-		this.rentedVehicles = rentedVehicles;
 		this.id = id;
 		this.zip = zip;
-		this.dailyRate = getRates(); // sets daily rate based on zip
+		this.inventory = new ArrayList<>();
 	}
 
-	/**
-	 * @return the id
-	 */
 	public int getId() {
 		return id;
 	}
 
-	/**
-	 * @return the zip
-	 */
 	public int getZip() {
 		return zip;
 	}
-
-	/**
-	 * @param zip the zip to set
-	 */
+	
 	public void setZip(int zip) {
 		this.zip = zip;
 	}
 
-	public int getVehicles() {
-		return vehicles;
+	public int availableVehicles() {
+		int availableVehicles = 0;
+		for(Vehicles vehicle : inventory ) {
+			if(vehicle.getStatus().equals("Available")) {
+				availableVehicles++;
+			}
+		}
+		
+		return availableVehicles;
 	}
-
-	public int getRentedVehicles() {
+	
+	public int rentedVehicles() {
+		int rentedVehicles = 0;
+		for(Vehicles vehicle : inventory ) {
+			if(vehicle.getStatus().equals("Rented")) {
+				rentedVehicles++;
+			}
+		}
+		
 		return rentedVehicles;
 	}
-
-	public void setRentedVehicles(int rentedVehicles) {
-		this.rentedVehicles = rentedVehicles;
+	
+	public int totalVehicles() {
+		return inventory.size();
 	}
-
-	public int availableVehicles() {
-		return vehicles - rentedVehicles;
-	}
+	
 
 	/**
 	 * @return the city
@@ -75,119 +72,6 @@ public class RentalLocations implements Serializable {
 		this.name = z;
 	}
 
-	public double getDailyRate() {
-		return dailyRate;
-	}
-
-	public void setDailyRate(double dailyRate) {
-		this.dailyRate = dailyRate;
-	}
-
-	public double getRates() {
-		if (type.equalsIgnoreCase("car")) {	
-			if ((zip >= 98001) && (zip <= 98100)) {
-				dailyRate = 27.50;
-			}
-			if ((zip >= 98101) && (zip <= 98200)) {
-				dailyRate = 22.49;
-			}
-			if ((zip >= 98201) && (zip <= 98300)) {
-				dailyRate = 31.00;
-			}
-			if ((zip >= 98301) && (zip <= 98400)) {
-				dailyRate = 12.25;
-			}
-			if ((zip >= 98401) && (zip <= 98500)) {
-				dailyRate = 75.24;
-			}
-			if ((zip >= 98501) && (zip <= 98600)) {
-				dailyRate = 34.99;
-			}
-			if ((zip >= 98601) && (zip <= 98700)) {
-				dailyRate = 28.99;
-			}
-			if ((zip >= 98701) && (zip <= 98800)) {
-				dailyRate = 33.00;
-			}
-			if ((zip >= 98801) && (zip <= 98900)) {
-				dailyRate = 19.99;
-			}
-			if ((zip >= 98901) && (zip <= 99000)) {
-				dailyRate = 9.99;
-			}
-		}
-		if (type.equalsIgnoreCase("truck")) {	
-			if ((zip >= 98001) && (zip <= 98100)) {
-				dailyRate = 55.00;
-			}
-			if ((zip >= 98101) && (zip <= 98200)) {
-				dailyRate = 44.98;
-			}
-			if ((zip >= 98201) && (zip <= 98300)) {
-				dailyRate = 61.49;
-			}
-			if ((zip >= 98301) && (zip <= 98400)) {
-				dailyRate = 24.49;
-			}
-			if ((zip >= 98401) && (zip <= 98500)) {
-				dailyRate = 98.74;
-			}
-			if ((zip >= 98501) && (zip <= 98600)) {
-				dailyRate = 69.99;
-			}
-			if ((zip >= 98601) && (zip <= 98700)) {
-				dailyRate = 56.49;
-			}
-			if ((zip >= 98701) && (zip <= 98800)) {
-				dailyRate = 62.79;
-			}
-			if ((zip >= 98801) && (zip <= 98900)) {
-				dailyRate = 27.99;
-			}
-			if ((zip >= 98901) && (zip <= 99000)) {
-				dailyRate = 28.99;
-			}
-		}
-		if (type.equalsIgnoreCase("van")) {	
-			if ((zip >= 98001) && (zip <= 98100)) {
-				dailyRate = 37.50;
-			}
-			if ((zip >= 98101) && (zip <= 98200)) {
-				dailyRate = 32.49;
-			}
-			if ((zip >= 98201) && (zip <= 98300)) {
-				dailyRate = 41.00;
-			}
-			if ((zip >= 98301) && (zip <= 98400)) {
-				dailyRate = 22.25;
-			}
-			if ((zip >= 98401) && (zip <= 98500)) {
-				dailyRate = 85.24;
-			}
-			if ((zip >= 98501) && (zip <= 98600)) {
-				dailyRate = 44.99;
-			}
-			if ((zip >= 98601) && (zip <= 98700)) {
-				dailyRate = 38.99;
-			}
-			if ((zip >= 98701) && (zip <= 98800)) {
-				dailyRate = 43.00;
-			}
-			if ((zip >= 98801) && (zip <= 98900)) {
-				dailyRate = 29.99;
-			}
-			if ((zip >= 98901) && (zip <= 99000)) {
-				dailyRate = 19.99;
-			}
-		}
-
-		return dailyRate;
-	}
-
-	public double total() {
-		
-		return rentedVehicles * dailyRate;
-	}
 
 	// Returns list of location names given zipcode
 	public List<String> getLocs(int zip, List<RentalLocations> r) {
@@ -212,3 +96,4 @@ public class RentalLocations implements Serializable {
 	 * System.out.println(list.toString()); // needs to be edited based on GUI } } }
 	 */
 }
+
