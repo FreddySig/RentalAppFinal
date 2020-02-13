918bb4de760b47d50ac5e263cee4da6fbb998dfc
meg <148425@G932LLAX.ustr.com>
2020-02-13 14:49:26 -0800

    testing to see if this is working yet....

---
+++ Sprint1_CarRental/src/Queries.java
@@ -0,0 +1,67 @@
+import java.text.NumberFormat;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.swing.JOptionPane;
+import javax.swing.table.DefaultTableModel;
+
+public class Queries {
+	
+	public static DefaultTableModel availableV(List<RentalLocations> list) {
+		String[] columns = { "ID", "Name", "Available Vehicles" };
+		Object[][] data = new Object[list.size()][3];
+
+		for (int i = 0; i < list.size(); i++) {
+			data[i][0] = list.get(i).getId();
+			data[i][1] = list.get(i).getName();
+			data[i][2] = list.get(i).availableVehicles();
+		}
+		DefaultTableModel dtm = new DefaultTableModel(data, columns);
+		return dtm;
+	}
+
+	public static DefaultTableModel ratesByLoc(List<RentalLocations> list) {
+		String name = JOptionPane.showInputDialog(null, "Input location name");
+		List<RentalLocations> locs = new ArrayList<>();
+		
+		for (RentalLocations r : list) {
+			if (r.getName().contentEquals(name)) {
+				locs.add(r);
+			}
+		}
+		
+		String[] columns = { "ID", "Name", "Daily Rate" };
+		Object[][] data = new Object[locs.size()][3];
+
+		for (int i = 0; i < locs.size(); i++) {
+			data[i][0] = locs.get(i).getId();
+			data[i][1] = locs.get(i).getName();
+			data[i][2] = locs.get(i).getRates();
+		}
+		DefaultTableModel dtm = new DefaultTableModel(data, columns);
+		return dtm;
+	}
+	
+	public static ArrayList<RentalLocations> filterLocationsByName(List<RentalLocations> list, String name) {
+		ArrayList<RentalLocations> locs = new ArrayList<>();
+		for (RentalLocations r : list) {
+			if (r.getName().contentEquals(name)) {
+				locs.add(r);
+			}
+		}
+		return locs;
+	}
+	
+	public static DefaultTableModel calculateRevenue(List<RentalLocations> list) {
+		String[] columns = { "ID", "Name", "Total Revenue" };
+		Object[][] data = new Object[list.size()][3];
+
+		for (int i = 0; i < list.size(); i++) {
+			data[i][0] = list.get(i).getId();
+			data[i][1] = list.get(i).getName();
+			data[i][2] = NumberFormat.getCurrencyInstance().format(list.get(i).total());
+		}
+		DefaultTableModel dtm = new DefaultTableModel(data, columns);
+		return dtm;
+	}
+}
---
+++ Sprint1_CarRental/src/RentalLocations.java
@@ -0,0 +1,154 @@
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+
+public class RentalLocations implements Serializable {
+	
+	private static final long serialVersionUID = 8743507069010815384L;
+	private String name; // rental location name,cities for locations
+	private int id;
+
+	private final int vehicles = 50;
+	private int rentedVehicles;
+
+	private double dailyRate;
+	private int zip;
+
+
+	// total vehicles = 50
+	public RentalLocations(String name, int rentedVehicles, int id, int zip) {
+		this.name = name;
+		this.rentedVehicles = rentedVehicles;
+		this.id = id;
+		this.zip = zip;
+		this.dailyRate = getRates(); // sets daily rate based on zip
+	}
+
+	/**
+	 * @return the id
+	 */
+	public int getId() {
+		return id;
+	}
+
+	/**
+	 * @return the zip
+	 */
+	public int getZip() {
+		return zip;
+	}
+
+	/**
+	 * @param zip the zip to set
+	 */
+	public void setZip(int zip) {
+		this.zip = zip;
+	}
+
+	public int getVehicles() {
+		return vehicles;
+	}
+
+	public int getRentedVehicles() {
+		return rentedVehicles;
+	}
+
+	public void setRentedVehicles(int rentedVehicles) {
+		this.rentedVehicles = rentedVehicles;
+	}
+
+	public int availableVehicles() {
+		return vehicles - rentedVehicles;
+	}
+
+	/**
+	 * @return the city
+	 */
+	public String getName() {
+		return name;
+	}
+
+	/**
+	 * @param city the city to set
+	 */
+	public void setName(String z) {
+		this.name = z;
+	}
+
+	public double getDailyRate() {
+		return dailyRate;
+	}
+
+	public void setDailyRate(double dailyRate) {
+		this.dailyRate = dailyRate;
+	}
+
+	public double getRates() {
+		if ((zip >= 98001) && (zip <= 98100)) {
+			dailyRate = 27.50;
+		}
+		if ((zip >= 98101) && (zip <= 98200)) {
+			dailyRate = 22.49;
+		}
+		if ((zip >= 98201) && (zip <= 98300)) {
+			dailyRate = 31.00;
+		}
+		if ((zip >= 98301) && (zip <= 98400)) {
+			dailyRate = 12.25;
+		}
+		if ((zip >= 98401) && (zip <= 98500)) {
+			dailyRate = 75.24;
+		}
+		if ((zip >= 98501) && (zip <= 98600)) {
+			dailyRate = 34.99;
+		}
+		if ((zip >= 98601) && (zip <= 98700)) {
+			dailyRate = 28.99;
+		}
+		if ((zip >= 98701) && (zip <= 98800)) {
+			dailyRate = 33.00;
+		}
+		if ((zip >= 98801) && (zip <= 98900)) {
+			dailyRate = 19.99;
+		}
+		if ((zip >= 98901) && (zip <= 99000)) {
+			dailyRate = 9.99;
+		}
+
+		return dailyRate;
+	}
+
+	public double total() {
+		
+		return rentedVehicles * dailyRate;
+	}
+
+	// Returns list of location names given zipcode
+	public List<String> getLocs(int zip, List<RentalLocations> r) {
+		List<String> list = new ArrayList<String>();
+		for (RentalLocations loc : r) {
+			if (loc.getZip() == zip) {
+				list.add(loc.getName());
+			}
+		}
+		return list;
+	}
+
+	// gets the rental rate given a name
+	public void locRates(String name, List<RentalLocations> rL) {
+		for (RentalLocations list : rL) {
+			if (list.getName() == (name)) {
+				System.out.println("Name:" + name + " " + list.getDailyRate()); // Needs to be edited based on GUI
+			}
+		}
+	}
+
+	// gets the details when given a location name
+	public void locDetails(String name, List<RentalLocations> rL) {// name of city desired
+		for (RentalLocations list : rL) {
+			if (list.getName() == name) {
+				System.out.println(list.toString()); // needs to be edited based on GUI
+			}
+		}
+	}
+}
