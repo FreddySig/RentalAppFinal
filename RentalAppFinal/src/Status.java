3fd8c13ad7098192b8db4b8db20bbd3dd618e84a
148647 <148647@G926LLAX.ustr.com>
2020-02-13 15:24:18 -0800

    Added toString to Status

--- RentalAppFinal/src/Status.java
+++ RentalAppFinal/src/Status.java
@@ -1,4 +1,14 @@
 
 public enum Status {
 	AVAILABLE, RENTED;
+
+	@Override
+	public String toString() {
+		switch (this) {
+		case AVAILABLE: return "Available";
+		case RENTED: return "Rented";
+		default: return "Status";
+		}
+	}
+	
 }
